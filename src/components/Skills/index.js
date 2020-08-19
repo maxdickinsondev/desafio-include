829830cf94e47e520f3e83ce89480cd051bd4f01
Max Dickinson <maxsantistadickinson@gmail.com>
2020-08-19 20:22:09 -0300

    Criando e estilizando seção de habilidades.

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,13 +1,84 @@
 import React from 'react';
 
-import { AreaSection, Container } from './styles';
+import { AreaSection, Container, Title,
+    Image, Content, Card, Box, Tech
+} from './styles';
+
+import html from '../../assets/images/skills/html-5.png';
+import css from '../../assets/images/skills/css.png';
+import js from '../../assets/images/skills/js.png';
+import react from '../../assets/images/skills/react.png';
+import node from '../../assets/images/skills/nodejs.png';
+import mysql from '../../assets/images/skills/mysql.png';
 
 export default function Skills() {
     return (
-        <AreaSection>
-            <Container>
+      <AreaSection>
+        <Container>
+            <Title>Skills</Title>
 
-            </Container>
-        </AreaSection>
+            <Content>
+                <Card>
+                    <Box>
+                        <Tech>HTML</Tech>
+                        <Image 
+                            src={html}
+                            alt="HTML"
+                        />
+                    </Box>
+                </Card>
+
+                <Card>
+                    <Box>
+                        <Tech>CSS</Tech>
+                        <Image 
+                            src={css}
+                            alt="HTML"
+                        />
+                    </Box>
+                </Card>
+
+                <Card>
+                    <Box>
+                        <Tech>Javascript</Tech>
+                        <Image 
+                            src={js}
+                            alt="HTML"
+                        />
+                    </Box>
+                </Card>
+
+                <Card>
+                    <Box>
+                        <Tech>React</Tech>
+                        <Image 
+                            src={react}
+                            alt="HTML"
+                        />
+                    </Box>
+                </Card>
+
+                <Card>
+                    <Box>
+                        <Tech>Node</Tech>
+                        <Image 
+                            src={node}
+                            alt="HTML"
+                        />
+                    </Box>
+                </Card>
+
+                <Card>
+                    <Box>
+                        <Tech>MySql</Tech>
+                        <Image 
+                            src={mysql}
+                            alt="HTML"
+                        />
+                    </Box>
+                </Card>
+            </Content>
+        </Container>
+      </AreaSection>  
     );
 }
--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -2,8 +2,81 @@ import styled from 'styled-components';
 
 export const AreaSection = styled.section`
     background: #595454;
+    float: left;
     width: 100vw;
-    height: 100vh;
+    padding: 80px 0px;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 90vw;
+    margin: 0 auto;
+
+    
+`;
+
+export const Title = styled.h3`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin-bottom: 70px;
+    color: #fefefe;
+
+    text-transform: uppercase;
+    letter-spacing: .5px;
+
+    position: relative;
+
+    &:before {
+        content: '';
+        position: absolute;
+        width: 6rem;
+        height: 0.2rem;
+        left: 50%;
+        top: 110%;
+        margin-left: -50px;
+        background-color: #fefefe;
+    }
+`;
+
+export const Content = styled.div`
+    float: left;
+    width: 100%;
+`;
+
+export const Image = styled.img`
+    width: 7rem;
+    height: 7rem;
+`;
+
+export const Card = styled.div`
+    float: left;
+    width: 33.33%;
+    padding: 15px;
+
+    @media (max-width: 767px) {
+        width: 50%;
+    }
+
+    @media (max-width: 500px) {
+        width: 100%;
+    }
+`;
+
+export const Box = styled.div`
+   background: #4a4a4a;
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+
+   padding: 20px;
+   border-radius: 0.3rem;
+`;
+
+export const Tech = styled.h3`
+    color: #f0f0f5;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+
+    font-size: 1.2rem;
+`;
